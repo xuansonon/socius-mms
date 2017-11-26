e00f37579ca0a8d9819a306f490590f655a3b695
xuansonon <hello@xuansonon.com>
2017-11-26 13:45:19 +1100

    create sidebar elements, add bootstrap jquery and fa icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+declare var $:any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
+
+  ngOnInit() {
+    $("body").ready(function(){
+      // alert("Hello world");
+      $(".nav > ul > li").click(function(){
+        $(".nav > ul > li").removeClass("selected");
+        $(this).addClass("selected");
+      });
+    });
+  }
+
 }
